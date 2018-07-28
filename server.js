61e8700eb75c99ddf0015fb2c27902db3931a725
Geoff Dabu <geoffdabu@gmail.com>
2018-07-27 17:57:59 -0700

    added server.js for heroku deployment

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
 
-var server = app.listen(app.get('port'), () => {
-  console.log('listening on port ', server.address().port);
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/dist/index.html`);
 });
+
+app.listen(PORT, error => (
+  error
+    ? console.error(error)
+    : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
+));
